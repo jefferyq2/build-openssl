9eef4cd84964cc92fd731f5bc055b563a55ec27d
Nathan Toone <nathan@toonetown.com>
2017-10-16 13:16:46 -0600

    Build targeting ios 8 or later.

--- configs/setup-ios.sh
+++ configs/setup-ios.sh
@@ -4,6 +4,6 @@ LIPO_PATH="$(which lipo)"
 GEN_SCRIPT="${CONFIGS_DIR}/common-clang-darwin.sh"
 
 CLANG_PATH="$(xcrun -f clang)"
-MIN_OS_VERSION="${MIN_OS_VERSION:-7.0}"
+MIN_OS_VERSION="${MIN_OS_VERSION:-8.0}"
 OPENSSL_CONFIGURE_NAME="iphoneos-cross"
 OPENSSL_PRECONFIGURE="${CONFIGS_DIR}/preconfigure-ios.sh"
