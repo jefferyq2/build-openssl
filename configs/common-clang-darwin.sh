aefe9fc17c15561b60379d178eaadea7281681ea
Nathan Toone <nathan@toonetown.com>
2017-11-14 11:39:08 -0700

    Don't include -fomit-frame-pointer for ios.armv7

--- configs/common-clang-darwin.sh
+++ configs/common-clang-darwin.sh
@@ -3,6 +3,6 @@
 SDK_FLAG="${SDK_VERSION_NAME}-version-min=${MIN_OS_VERSION}"
 
 export CC="${CLANG_PATH}"
-export CFLAGS="-arch ${ARCH} -m${SDK_FLAG} -fPIC -fembed-bitcode -isysroot ${SDK_PATH}"
+export CFLAGS="-arch ${ARCH} -m${SDK_FLAG} -fPIC -fembed-bitcode -isysroot ${SDK_PATH} ${COMP_FLAGS}"
 export LDFLAGS="-arch ${ARCH} -m${SDK_FLAG} -isysroot ${SDK_PATH}"
 export CC="${CC} ${CFLAGS}"
