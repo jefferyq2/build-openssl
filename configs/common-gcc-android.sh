e8200321ffc2c1dca8d68bb671a70d002d103fe8
Nathan Toone <nathan@toonetown.com>
2017-12-08 08:40:27 -0700

    Fix to build with the latest android NDK (see https://android.googlesource.com/platform/ndk.git/+/master/docs/UnifiedHeaders.md)

--- configs/common-gcc-android.sh
+++ configs/common-gcc-android.sh
@@ -2,9 +2,9 @@
 
 PFIX="${ANDROID_NDK_HOME}/toolchains/${GCC_ARCH}-${ANDROID_GCC_VERSION}/prebuilt/darwin-x86_64/bin/${GCC_PREFIX}"
 
-export SYSROOT="${ANDROID_NDK_HOME}/platforms/android-${ANDROID_PLATFORM}/arch-${PLATFORM_ARCH}"
-export CROSS_SYSROOT="${SYSROOT}"
-export ANDROID_DEV="${CROSS_SYSROOT}/usr"
+export SYSROOT="${ANDROID_NDK_HOME}/sysroot"
+export PLATROOT="${ANDROID_NDK_HOME}/platforms/android-${ANDROID_PLATFORM}/arch-${PLATFORM_ARCH}"
+export SYSINC="${SYSROOT}/usr/include/${GCC_PREFIX}"
 export CC="${PFIX}-gcc"
 export RANLIB="${PFIX}-ranlib"
 export AR="${PFIX}-ar"
@@ -13,8 +13,8 @@ export CPP="${PFIX}-cpp"
 export CXX="${PFIX}-g++"
 export LD="${PFIX}-ld"
 export STRIP="${PFIX}-strip"
-export CFLAGS="--sysroot=${CROSS_SYSROOT} ${COMP_FLAGS}"
-export LDFLAGS="--sysroot=${CROSS_SYSROOT}"
+export CFLAGS="--sysroot=${SYSROOT} -isystem \"${SYSINC}\" -D__ANDROID_API__=${ANDROID_PLATFORM} ${COMP_FLAGS}"
+export LDFLAGS="--sysroot=${PLATROOT}"
 
 ANDROID_BUILD_PIE="${ANDROID_BUILD_PIE:-true}"
 if [ "${ANDROID_BUILD_PIE}" == "true" ]; then
