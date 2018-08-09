b52d2a730c81efd483d312007bcc186293e02b4e
Nathan Toone <nathan@toonetown.com>
2018-08-09 14:57:38 -0600

    Build targeting ios 9.0 or later

--- configs/setup-ios.sh
+++ configs/setup-ios.sh
@@ -4,7 +4,7 @@ LIPO_PATH="$(which lipo)"
 GEN_SCRIPT="${CONFIGS_DIR}/common-clang-darwin.sh"
 
 CLANG_PATH="$(xcrun -f clang)"
-MIN_OS_VERSION="${MIN_OS_VERSION:-8.0}"
+MIN_OS_VERSION="${MIN_OS_VERSION:-9.0}"
 OPENSSL_CONFIGURE_NAME="iphoneos-cross"
 OPENSSL_PRECONFIGURE="${CONFIGS_DIR}/preconfigure-ios.sh"
 COMP_FLAGS="-fomit-frame-pointer"
