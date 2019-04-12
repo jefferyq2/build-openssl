33e80c450272f99a6124553d067d2b6e10913b8a
Nathan Toone <nathan@toonetown.com>
2019-04-11 18:05:35 -0600

    Fix problem with redirection syntax

--- configs/setup-windows.bat
+++ configs/setup-windows.bat
@@ -1,7 +1,7 @@
 @echo off
 IF NOT EXIST "%VCVAR_DIR%\vcvarsall.bat" (
-    echo Cannot set VCVARS from "%VCVAR_DIR%\vcvarsall.bat"> 1&2
-    echo. 1>2
+    echo Cannot set VCVARS from "%VCVAR_DIR%\vcvarsall.bat" 1>&2
+    echo. 1>&2
     exit /B 1
 )
 IF "%MIN_WIN_NT_VERSION%"=="" SET MIN_WIN_NT_VERSION=0x0A00
